5db323b576b01508718569c0c61c0195b13b90cb
bariscankurt <bariscankurt0@gmail.com>
2020-09-18 20:10:35 +0300

    new appbar, sign in and register visual

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,7 +7,7 @@ import 'package:guzel_ama_ingilicce/screens/profile/profile.dart';
 import 'package:guzel_ama_ingilicce/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:flutter/cupertino.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -35,8 +35,8 @@ class _HomeState extends State<Home> {
       value: DatabaseService().fruits,
       child: Scaffold(
         appBar: AppBar(
-          title: Text("Hoşgeldiniz - Oturum Açıldı"),
-          backgroundColor: Colors.blue[300],
+          title: Text("Hoşgeldiniz - Oturum Açıldı",style: TextStyle(fontSize: 16.0),),
+          backgroundColor: CupertinoColors.systemBlue,
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
